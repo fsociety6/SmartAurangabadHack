7979f646a0f61218abd86d58194fa356c6750365
Shubham Sanjiv Naik <naikshub8412@gmail.com>
2020-01-21 11:44:53 +0530

    UserModel Added

--- CCWebsite/chat/consumers.py
+++ CCWebsite/chat/consumers.py
@@ -3,3 +3,14 @@ import json
 from django.contrib.auth import get_user_model
 from channels.consumer import AsyncConsumer
 from channels.db import database_sync_to_async
+
+
+class ChatConsumer(AsyncConsumer):
+    def websocket_connect(self, event):
+        print("connected: ", event)
+
+    def websocket_receive(self, event):
+        print("Received : ", event)
+
+    def websocket_disconnect(self, event):
+        print("Disconnected: ", event)
